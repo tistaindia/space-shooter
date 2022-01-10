1f1366db8dbd4872d43772adc6ced90c893d1e48
tistaindia <97467113+tistaindia@users.noreply.github.com>
2022-01-10 22:53:20 +0530

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,78 @@
+controller.A.onEvent(ControllerButtonEvent.Pressed, function () {
+    projectile = sprites.createProjectileFromSprite(img`
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . 3 3 3 3 . . . 
+        . . . . . . . 3 3 3 8 3 3 . . . 
+        . . . . . . . 3 8 8 3 8 3 . . . 
+        . . 3 3 3 3 3 3 3 3 3 3 3 . . . 
+        . . . . . . . . 3 3 3 3 . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        `, mySprite, 200, 0)
+})
+sprites.onOverlap(SpriteKind.Projectile, SpriteKind.Enemy, function (sprite, otherSprite) {
+    sprite.destroy()
+    otherSprite.destroy()
+    info.changeScoreBy(1)
+})
+sprites.onOverlap(SpriteKind.Player, SpriteKind.Enemy, function (sprite, otherSprite) {
+    info.changeLifeBy(-1)
+    otherSprite.destroy(effects.disintegrate, 500)
+    scene.cameraShake(4, 500)
+})
+let Enemy_Ship: Sprite = null
+let projectile: Sprite = null
+let mySprite: Sprite = null
+effects.starField.startScreenEffect()
+mySprite = sprites.create(img`
+    ....ffffff.........ccc..
+    ....ff22ccf.......cc4f..
+    .....ffccccfff...cc44f..
+    ....cc24442222cccc442f..
+    ...c9b4422222222cc422f..
+    ..c9999b222222222222fc..
+    .c2b991119222222222c22c.
+    c2222b11992222ccccccc22f
+    f222222222222c222ccfffff
+    .f2222222222444222f.....
+    ..ff2222222cf444222f....
+    ....ffffffffff444222c...
+    .........f2cfffc2222c...
+    .........fcc2ffffffff...
+    ..........fc2ffff.......
+    ...........fffff........
+    `, SpriteKind.Player)
+controller.moveSprite(mySprite)
+mySprite.setStayInScreen(true)
+info.setLife(5)
+game.onUpdateInterval(2000, function () {
+    Enemy_Ship = sprites.create(img`
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . 2 2 2 2 . . . 
+        . . . . . . . 2 2 1 1 1 1 2 . . 
+        . . . . 2 2 3 3 1 1 1 1 1 1 . . 
+        . . 3 3 3 3 1 1 1 1 1 1 1 1 . . 
+        . . 1 1 1 1 1 1 1 1 1 1 1 1 . . 
+        . . 3 3 2 2 3 1 1 1 1 1 1 1 . . 
+        . . . . . . 2 2 3 1 1 1 1 2 . . 
+        . . . . . . . . . 2 2 2 2 . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        `, SpriteKind.Enemy)
+    Enemy_Ship.x = scene.screenWidth()
+    Enemy_Ship.vx = -20
+    Enemy_Ship.y = randint(10, scene.screenHeight() - 0)
+})
